1ebd061db82be82493c0c57e0aa4b00132e8bb16
22120217 <hoanglenam0905@gmail.com>
2024-12-25 20:43:39 +0700

    Refactor code

---
+++ Models/Payment/Enums/PaymentMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_Store_Management_App.Models.Payment.Enums
+{
+    /// <summary>
+    /// Enum xác định phương thức thanh toán.
+    /// </summary>
+    public enum PaymentMethod
+    {
+        Momo,
+        VNPay,
+        Paypal,
+        ZaloPay,
+        Demo
+    }
+}
---
+++ Services/Payment/Interfaces/IPaymentStrategy.cs
@@ -0,0 +1,18 @@
+﻿using Books_Store_Management_App.Models.Payment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_Store_Management_App.Services.Payment.Interfaces
+{
+    /// <summary>
+    /// Interface xác định các phương thức cần cung cấp cho một dịch vụ thanh toán.
+    /// </summary>
+    public interface IPaymentStrategy
+    {
+        Task<PaymentResult> ProcessPayment(PaymentRequest request);
+        Task<PaymentResult> QueryOrder(string appTransId);
+    }
+}
